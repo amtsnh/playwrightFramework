1e71d258f959fde3a49b0d091e378e96a525dd84
AMIT KUMAR SINHA <154301797+amtsnh@users.noreply.github.com>
2024-03-08 16:55:07 +0530

    Create testContext.ts

---
+++ testContext.ts
@@ -0,0 +1,11 @@
+import { Logger } from "winston";
+
+const testCotext = {
+    //@ts-ignore
+    logger: undefined as Logger,
+    runtimeStorageFile: undefined as string,
+    assertsJson: undefined as any,
+    runtimeLoggerFile: undefined as string
+}
+export default testCotext;
+    
